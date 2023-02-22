941e461964063d76aa4ca94262702467c46e9ada
LucasGonzalezSwd <lucas.ing.ind@gmail.com>
2023-02-22 10:57:43 -0300

    in progress

--- client/src/components/ContactMe.jsx
+++ client/src/components/ContactMe.jsx
@@ -5,8 +5,8 @@ export const ContactMe = () => {
   return (
     <div className="flex flex-row w-screen box-border bg-gradient-to-r from-gray-600 to-gray-900">
       <NavBar/>
-      <div className=" flex justify-center items-center  w-screen h-screen ">
-
+      <div className=" flex justify-center items-center  w-screen h-screen  text-red-600 text-3xl">
+      IN PROGRESS
       </div>
     </div>
   )
--- client/src/components/Projects.jsx
+++ client/src/components/Projects.jsx
@@ -4,8 +4,8 @@ export const Projects = () => {
   return (
     <div className="flex flex-row w-screen box-border bg-gradient-to-r from-gray-600 to-gray-900">
       <NavBar/>
-      <div className=" flex justify-center items-center  w-screen h-screen ">
-
+      <div className=" flex justify-center items-center  w-screen h-screen text-3xl text-red-600">
+              IN PROGRESS
       </div>
     </div>
   )
